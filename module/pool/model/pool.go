e84851f3e03de47b998c8874dd71750619323341
Khoi Nguyen <khoi.ndq@gmail.com>
2023-06-17 16:10:05 +0700

    Add explain for pool model

--- module/pool/model/pool.go
+++ module/pool/model/pool.go
@@ -10,16 +10,18 @@ const EntityName = "Pool"
 
 var FakePoolDB map[int64]Pool
 
-// Pool object
+// Pool represents a pool with an ID and a collection of values.
 type Pool struct {
-	PoolID     int64     `json:"poolId" binding:"required"`
-	PoolValues []float64 `json:"poolValues" binding:"required"`
+	PoolID     int64     `json:"poolId" binding:"required"`     // ID of the pool
+	PoolValues []float64 `json:"poolValues" binding:"required"` // ID of the pool
 }
 
+// PoolInsert is used for inserting a pool.
 type PoolInsert struct {
-	Pool
+	Pool // Embedding the Pool struct for reuse
 }
 
+// Validate validates the PoolInsert struct.
 func (pool *PoolInsert) Validate() error {
 	if len(pool.PoolValues) == 0 {
 		return ErrPoolCreateValidateFailed(errors.New("pool.poolValues is empty"))
@@ -27,25 +29,30 @@ func (pool *PoolInsert) Validate() error {
 	return nil
 }
 
+// PoolAppend is used for appending to a pool.
 type PoolAppend struct {
-	Pool
+	Pool // Embedding the Pool struct for reuse
 }
 
+// Validate validates the PoolAppend struct.
 func (pool *PoolAppend) Validate() error {
 	//nothing to validate here
 	return nil
 }
 
-// PoolQuantileGet object
+// PoolQuantileGet represents a request to get the quantile of a pool.
 type PoolQuantileGet struct {
-	PoolID     int64   `json:"poolId" binding:"required"`
-	Percentile float64 `json:"percentile" binding:"required"`
-}
-type PoolQuantileResp struct {
-	TotalCount         int64   `json:"totalCount"`
-	CalculatedQuantile float64 `json:"calculatedQuantile"`
+	PoolID     int64   `json:"poolId" binding:"required"`     // ID of the pool
+	Percentile float64 `json:"percentile" binding:"required"` // Desired percentile
 }
 
+// PoolQuantileResp represents the response containing the quantile information.
+type PoolQuantileResp struct {
+	TotalCount         int64   `json:"totalCount"`         // Total count of values in the pool
+	CalculatedQuantile float64 `json:"calculatedQuantile"` // Calculated quantile value
+}
+
+// Validate validates the PoolQuantileGet struct.
 func (pool *PoolQuantileGet) Validate() error {
 	if pool.Percentile < 0 || pool.Percentile > 100 {
 		return ErrPoolPercentileGetValidateFailed(errors.New("pool.Percentile must be between 0 and 100"))
@@ -53,15 +60,19 @@ func (pool *PoolQuantileGet) Validate() error {
 	return nil
 }
 
+// PoolStatus represents the status of inserted or appended pool
 type PoolStatus string
 
+// PoolStatus constants for different pool statuses.
 const PoolStatusInserted = PoolStatus("inserted")
 const PoolStatusAppended = PoolStatus("appended")
 
+// ToPointer converts a PoolStatus value to a pointer.
 func (p PoolStatus) ToPointer() *PoolStatus {
 	return &p
 }
 
+// ErrPoolPercentileGetValidateFailed creates a custom error for failed percentile validation.
 func ErrPoolPercentileGetValidateFailed(err error) *common.AppError {
 	return common.NewCustomError(
 		err,
@@ -70,6 +81,7 @@ func ErrPoolPercentileGetValidateFailed(err error) *common.AppError {
 	)
 }
 
+// ErrPoolCreateValidateFailed creates a custom error for failed pool creation validation.
 func ErrPoolCreateValidateFailed(err error) *common.AppError {
 	return common.NewCustomError(
 		err,
@@ -78,6 +90,7 @@ func ErrPoolCreateValidateFailed(err error) *common.AppError {
 	)
 }
 
+// ErrPoolNotFound creates a custom error for a pool not found.
 func ErrPoolNotFound(err error) *common.AppError {
 	return common.NewCustomError(
 		err,
